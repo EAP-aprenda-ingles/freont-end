9727be82a5f494a171b189dc6689e22465616556
OtavioMaldaner <otaviomaldaner571@gmail.com>
2023-10-29 16:40:40 -0300

    upload

--- eap/src/app/register/page.tsx
+++ eap/src/app/register/page.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+import logo from "../../../public/logo-sem-fundo.png";
 export default function Register() {
-  return <main></main>;
+  return (
+    <main>
+      <div>
+        <Image
+          src={logo}
+          alt="logo"
+          width={60}
+          height={60}
+          quality={100}
+          placeholder="blur"
+        />
+      </div>
+    </main>
+  );
 }
